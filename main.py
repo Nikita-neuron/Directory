70a3fcff6daedf34a866e88a69163ccb7bd7e78d
ldkkbb <nkitin2017@mail.ru>
2022-03-30 18:12:45 +0300

    Updating features

--- main.py
+++ main.py
@@ -19,13 +19,25 @@ img = 0
 @app.route("/getStudent", methods=["GET"])
 def getStud():
     data = request.args
-    bd.get_student_by_id(int(data["id"]))
+    return bd.get_student_by_id(int(data["id"]))
 
 
 @app.route("/getTeacher", methods=["GET"])
 def getTeach():
     data = request.args
-    bd.get_teacher_by_id(int(data["id"]))
+    return bd.get_teacher_by_id(int(data["id"]))
+
+
+@app.route("/getGroup", methods=["GET"])
+def getGroup():
+    data = request.args
+    return bd.get_group_by_id(int(data["id"]))
+
+
+@app.get("/getSubject", methods=["GET"])
+def getSubj():
+    data = request.args
+    return bd.get_subject_by_id(int(data["id"]))
 
 
 @app.route("/addTeacher", methods=["POST"])
@@ -38,6 +50,11 @@ def addTeach():
 def updateTeach():
     data = request.form
     bd.update_teacher(int(data["id"]), data["surname"], data["name"], data["patronymic"])
+
+
+@app.route("/getAllTeachers")
+def allTeach():
+    return bd.get_all_teachers()
 
 
 @app.route("/updateStud", methods=["POST"])
@@ -62,6 +79,11 @@ def add():
                        img, data["date_of_birth"], data["characteristic"])
 
 
+@app.route("/getAllStudents")
+def allStud():
+    return bd.get_all_students()
+
+
 @app.route("/updateGroup", methods=["POST"])
 def updateGroup():
     data = request.form
@@ -72,6 +94,28 @@ def updateGroup():
 def addGroup():
     data = request.form
     bd.add_group(data["name"], int(data["id_headman"]))
+
+
+@app.route("/getAllGroups")
+def allGroups():
+    return bd.get_all_groups()
+
+
+@app.route("/updateSubject", methods=["POST"])
+def updateSub():
+    data = request.form
+    bd.update_subject(int(data["id"]), data["name"])
+
+
+@app.route("/addSubject", methods=["POST"])
+def addSub():
+    data = request.form
+    bd.add_subject(data["name"])
+
+
+@app.route("/getAllSubjects")
+def getSub():
+    return bd.get_all_subjects()
 
 
 @app.route("/schedule")
