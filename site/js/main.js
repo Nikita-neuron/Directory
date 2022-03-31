871442d74af6c0e62ce115737a759fc1a65baa20
Izolenta1 <geor.novickoff@yandex.ru>
2022-03-31 22:03:34 +0300

    Ver a1.3

--- site/js/main.js
+++ site/js/main.js
@@ -117,8 +117,21 @@ window.onload = function(){
     const StudentAddPersonBTN = document.getElementById("StudentAddPersonBTN");
     const StudentEditWindowExitBTN = document.getElementById("StudentEditWindowExitBTN");
 
+    const StudentInfoTable1 = document.getElementById("StudentInfoTable1");
+    const StudentInfoTable2 = document.getElementById("StudentInfoTable2");
+    const StudentInfoTable3 = document.getElementById("StudentInfoTable3");
+    const StudentEditTable = document.getElementById("StudentEditTable");
+
     StudentEditInfoBTN.onclick = function() {
         StudentEditWindow.style.display = "flex";
+
+        StudentEditTable.children[0].children[0].children[1].innerHTML = StudentInfoTable1.children[0].children[0].children[1].innerHTML
+
+        for (let i = 1; i < StudentInfoTable2.children[0].children.length + 1; i++) {
+            StudentEditTable.children[0].children[i].children[1].innerHTML = StudentInfoTable2.children[0].children[i-1].children[1].innerHTML
+        }
+
+        StudentEditTable.children[0].children[6].children[1].innerHTML = StudentInfoTable3.children[0].children[0].children[1].innerHTML
     }
 
     StudentAddPersonBTN.onclick = function() {
@@ -127,6 +140,10 @@ window.onload = function(){
 
     StudentEditWindowExitBTN.onclick = function() {
         StudentEditWindow.style.display = "none";
+
+        for (let i = 0; i < StudentEditTable.children[0].children.length - 1; i++) {
+            StudentEditTable.children[0].children[i].children[1].innerHTML = "";
+        }
     }
 
 
@@ -136,8 +153,21 @@ window.onload = function(){
     const TeacherAddPersonBTN = document.getElementById("TeacherAddPersonBTN");
     const TeacherEditWindowExitBTN = document.getElementById("TeacherEditWindowExitBTN");
 
+    const TeacherInfoTable1 = document.getElementById("TeacherInfoTable1");
+    const TeacherInfoTable2 = document.getElementById("TeacherInfoTable2");
+    const TeacherInfoTable3 = document.getElementById("TeacherInfoTable3");
+    const TeacherEditTable = document.getElementById("TeacherEditTable");
+
     TeacherEditInfoBTN.onclick = function() {
         TeacherEditWindow.style.display = "flex";
+
+        TeacherEditTable.children[0].children[0].children[1].innerHTML = TeacherInfoTable1.children[0].children[0].children[1].innerHTML
+
+        for (let i = 1; i < TeacherInfoTable2.children[0].children.length + 1; i++) {
+            TeacherEditTable.children[0].children[i].children[1].innerHTML = TeacherInfoTable2.children[0].children[i-1].children[1].innerHTML
+        }
+
+        TeacherEditTable.children[0].children[5].children[1].innerHTML = TeacherInfoTable3.children[0].children[0].children[1].innerHTML
     }
 
     TeacherAddPersonBTN.onclick = function() {
@@ -146,6 +176,10 @@ window.onload = function(){
 
     TeacherEditWindowExitBTN.onclick = function() {
         TeacherEditWindow.style.display = "none";
+
+        for (let i = 0; i < TeacherEditTable.children[0].children.length - 1; i++) {
+            TeacherEditTable.children[0].children[i].children[1].innerHTML = "";
+        }
     }
 
 
@@ -156,8 +190,18 @@ window.onload = function(){
     const AddGroupBTN = document.getElementById("AddGroupBTN");
     const GroupEditWindowExitBTN = document.getElementById("GroupEditWindowExitBTN");
 
+    const GroupInfoTable1 = document.getElementById("GroupInfoTable1");
+    const GroupInfoTable2 = document.getElementById("GroupInfoTable2");
+    const GroupEditTable = document.getElementById("GroupEditTable");
+
     EditGroupBTN.onclick = function() {
         GroupEditWindow.style.display = "flex";
+
+        GroupEditTable.children[0].children[0].children[1].innerHTML = GroupInfoTable1.children[0].children[0].children[1].innerHTML
+
+        for (let i = 1; i < GroupInfoTable2.children[0].children.length + 1; i++) {
+            GroupEditTable.children[0].children[i].children[1].innerHTML = GroupInfoTable2.children[0].children[i-1].children[1].innerHTML
+        }
     }
 
     AddGroupBTN.onclick = function() {
@@ -166,6 +210,10 @@ window.onload = function(){
 
     GroupEditWindowExitBTN.onclick = function() {
         GroupEditWindow.style.display = "none";
+
+        for (let i = 0; i < GroupEditTable.children[0].children.length; i++) {
+            GroupEditTable.children[0].children[i].children[1].innerHTML = "";
+        }
     }
 
     const SubjectEditWindow = document.getElementById("SubjectEditWindow");
@@ -173,8 +221,25 @@ window.onload = function(){
     const AddSubjectBTN = document.getElementById("AddSubjectBTN");
     const SubjectEditWindowExitBTN = document.getElementById("SubjectEditWindowExitBTN");
 
+    const SubjectInfoTable1 = document.getElementById("SubjectInfoTable1");
+    const SubjectInfoTable2 = document.getElementById("SubjectInfoTable2");
+    const SubjectInfoTable3 = document.getElementById("SubjectInfoTable3");
+    const SubjectInfoTable4 = document.getElementById("SubjectInfoTable4");
+    const SubjectInfoTable5 = document.getElementById("SubjectInfoTable5");
+    const SubjectEditTable = document.getElementById("SubjectEditTable");
+
     EditSubjectBTN.onclick = function() {
         SubjectEditWindow.style.display = "flex";
+
+        SubjectEditTable.children[0].children[0].children[1].innerHTML = SubjectInfoTable1.children[0].children[0].children[1].innerHTML;
+
+        for (let i = 1; i < SubjectInfoTable2.children[0].children.length + 1; i++) {
+            SubjectEditTable.children[0].children[i].children[1].innerHTML = SubjectInfoTable2.children[0].children[i-1].children[1].innerHTML
+        }
+
+        SubjectEditTable.children[0].children[3].children[1].innerHTML = SubjectInfoTable3.children[0].children[0].children[1].innerHTML;
+        SubjectEditTable.children[0].children[4].children[1].innerHTML = SubjectInfoTable4.children[0].children[0].children[1].innerHTML;
+        SubjectEditTable.children[0].children[5].children[1].innerHTML = SubjectInfoTable5.children[0].children[0].children[1].innerHTML;
     }
 
     AddSubjectBTN.onclick = function() {
@@ -183,5 +248,10 @@ window.onload = function(){
 
     SubjectEditWindowExitBTN.onclick = function() {
         SubjectEditWindow.style.display = "none";
+
+        for (let i = 0; i < SubjectEditTable.children[0].children.length; i++) {
+            SubjectEditTable.children[0].children[i].children[1].innerHTML = "";
+        }
     }
 };
+
